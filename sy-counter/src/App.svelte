9db6a3f2f7388ca09d9e06011e817c1fba797a78
Ghislaine Lykens <glykens@Ghislaines-MacBook-Pro.local>
2021-12-22 18:57:45 -0500

    working

---
+++ sy-counter/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Counter from './Counter.svelte'
+	</script>
+	
+	<main>
+		<div id="page-container">
+		<div class = "header-container">
+		<h1>SY Counter</h1>
+		</div>
+		<Counter />
+	</div>
+	</main>
+	
+	
+	<style>
+	
+		#page-container {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	
+		.header-container {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	
+		h1{
+			display: inline-flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 67px;
+			letter-spacing: 34px;
+			width: 70%;
+			height: 20vh;
+			color: #31fde2;
+			background-color: #007fff;
+			border-style: double;
+		}
+	
+	</style>
+	
+	
---
+++ sy-counter/src/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+    let count = 0;
+  
+    function handleClick() {
+      count += 1;
+    }
+  </script>
+  
+  <button 
+   on:click={handleClick}>
+    Ouch! You have poked me {count}
+    {count === 1 ? "time" : "times"}
+  </button>
